2425e0e1c62783aa24d6b39d9085464842ce40df
shubham <skverma7543@gmail.com>
2022-08-19 23:16:18 +0530

    test2

--- index.js
+++ index.js
@@ -2,32 +2,11 @@ const { MongoClient } = require("mongodb");
 const express=require('express')
 const app=express()
 
-const port=process.env.port || 4202
-// Replace the uri string with your connection string.
-const uri =
-  "mongodb+srv://javascript:javascript@cluster0.a9uny.mongodb.net/?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri);
-var data=null;
-async function run() {
-  try {
-    const database = client.db('test');
-    const movies = database.collection('Student');
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { city: 'chaibasa' };
-    const movie = await movies.find(query).toArray();
-    data=movie
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
-run().catch(console.dir);
-
+const port=process.env.port||4202
 app.get('/student',(req,res)=>{
+   let data={
+    name:"shubham"
+   }
     res.write(JSON.stringify(data))
     res.end()
 }).listen(port)
